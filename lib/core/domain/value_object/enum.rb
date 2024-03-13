ff966aae53b0f601660367d1eb5bb20554757d17
JuanSAuzaO <sebastianauza@hotmail.com>
2024-03-13 17:42:21 -0500

    Added Enum validator

--- lib/core/domain/value_object/enum.rb
+++ lib/core/domain/value_object/enum.rb
@@ -3,11 +3,12 @@
 module Core
   module Domain
     module ValueObject
-      class String
-        # @param value [String]
+      class Enum
+        VALUES = [].freeze
+
         def initialize(value)
           @value = value
-          ensure_valid_string
+          ensure_valid_value
         end
 
         # @return [String]
@@ -15,8 +16,10 @@ module Core
 
         private
 
-        def ensure_valid_string
-          raise InvalidStringError, self.class.name.demodulize if @value.strip.empty?
+        def ensure_valid_value
+          return if self.class::VALUES.include?(@value)
+
+          raise InvalidEnumError, self.class.name.demodulize
         end
       end
     end
