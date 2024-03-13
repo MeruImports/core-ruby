f2777339bc878e074ece1fe00c67527a4868136a
JuanSAuzaO <sebastianauza@hotmail.com>
2024-03-13 12:20:35 -0500

    Improving String validator

--- lib/core/domain/value_object/string.rb
+++ lib/core/domain/value_object/string.rb
@@ -4,9 +4,17 @@ module Core
   module Domain
     module ValueObject
       class String
+        MESSAGE_STARTERS = {
+            owner_id: 'El id del propietario',
+            license_plate: 'La placa',
+            make: 'La marca',
+            model: 'El modelo'
+          }
         # @param value [String]
-        def initialize(value, name, message)
+        def initialize(value)
           @value = value
+          name = formatter[0]
+          message = formatter[1]
           ensure_valid_string(name, message)
         end
 
@@ -17,6 +25,13 @@ module Core
 
         def ensure_valid_string(name, message)
           raise InvalidStringError, {name:, message:} if @value.strip.empty?
+        end
+
+        def formatter
+          children_class_name = self.class.name.split('::').last
+          name = children_class_name.gsub(/([a-z\d])([A-Z])/, '\1_\2').downcase
+          message = MESSAGE_STARTERS[name.to_sym]
+          [name, message]
         end
       end
     end
